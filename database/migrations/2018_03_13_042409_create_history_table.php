7c045e81b97ec640ef57dfb766c13b045bbbc0c4
ThanhNguyenFG <37108561+ThanhNguyenFG@users.noreply.github.com>
2018-03-13 11:46:03 +0700

    Them table History (Thanh)

---
+++ database/migrations/2018_03_13_042409_create_history_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_Id');
+            $table->unsignedInteger('contest_Id');
+            $table->unsignedInteger('question_Id');
+            $table->unsignedInteger('answer_Id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history');
+    }
+}
