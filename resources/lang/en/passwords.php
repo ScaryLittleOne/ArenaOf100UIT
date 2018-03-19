fb954c4907febfb9e433c2cef26e5295c8d397a9
ScaryLittleOne <ngToan1107@gmail.com>
2018-03-19 15:06:57 +0700

    Change The Language Of Message Wrong Username/Password

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'password' => 'Password được tạo ít nhất cũng phải 6 kí tự.',
+    'reset' => 'Mật Khẩu CỦa BẠn Đã Được Reset',
+    'sent' => 'We have username your password reset link!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "We can't find a user with that username",
 
 ];
