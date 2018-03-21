dd46042ca9cabd28cb286581a5cab5689e21ae7b
conlocxoaythuytrieu <conlocxoaythuytrieu@gmail.com>
2018-03-21 12:30:54 +0700

    Revert "dong ho giao dien"
    This reverts commit 05894f1aa043e594d23ce64e887ef174b0ef5b98.

--- resources/views/UserContest.blade.php
+++ resources/views/UserContest.blade.php
@@ -2,8 +2,9 @@
 @section('title','Đấu trường 100')
 @section('style')
 .dongho{
-padding: 40px 40px;
+	padding: 40px 40px;
 	border-radius: 70px;
+	background: #fff;
 	font-size: 30px;
 }
 @stop
@@ -15,32 +16,40 @@ padding: 40px 40px;
 </button>
 <button class="dongho" id='click'>BẮT ĐẦU</button>
 </center>
-<span id="time" style="display: none;"></span>
 <script type="text/javascript">
-   $("#click").click(function(e){
-        time_dest = 0;
-        e.preventDefault();
-        $("#time").load("http://arena.test/user #ry");
-        var ryry = setInterval( function() {
-            $("#time").load("http://arena.test/user #ry");
-            if (time_dest == 0) 
-                time_dest = $("#ry").html();
-            time_curr = $("#ry").html();
-            time_dem = time_count(time_dest, time_curr)
-            $("#dem").html(time_dem);
-            if (time_dem<=0) clearInterval(ryry);
-          },1000);
-      function time_count(time_begin, time_curr) {
-        time_limit = 10;
-        spl_time_begin = time_begin.split("-");
-        spl_time_curr = time_curr.split("-");
-        time_begin = new Date(spl_time_begin[0],spl_time_begin[1],spl_time_begin[2],spl_time_begin[3],spl_time_begin[4],spl_time_begin[5]);
-        time_curr = new Date(spl_time_curr[0],spl_time_curr[1],spl_time_curr[2],spl_time_curr[3],spl_time_curr[4],spl_time_curr[5]);
-        return time_limit - (time_curr - time_begin)/1000;
-
-        //...
-      }
-});
-
+	var thoigian = 30;
+var donvi = "giây";
+var khoangcach = 1000; // =1s
+if (donvi == "phút") khoangcach = 60000; //=1 phút
+var bandau = thoigian;
+document.getElementById("dem").innerHTML = " "+thoigian.toString();
+document.getElementById("click").addEventListener("click", opennewtab, false);
+document.getElementById("demnguoc").style.display = 'none';
+document.getElementById("donvi").innerHTML = donvi;
+function demlui() {
+    document.getElementById("click").style.display = 'none';
+    document.getElementById("demnguoc").style.display = 'block';
+    var timer = setInterval(function () {
+        thoigian--;
+        if (thoigian < 0) {
+            document.getElementById("demnguoc").style.display = 'none';
+            document.getElementById("click").style.display = 'block';
+            clearInterval(timer);
+            document.getElementById("dem").innerHTML = " "+bandau.toString();
+            thoigian = bandau;
+        } else {
+            document.getElementById("dem").innerHTML = " "+thoigian.toString();
+        }
+    }, khoangcach);
+};
+document.getElementById("click").onclick = demlui;
+function opennewtab(){
+    var a = document.createElement("a");
+    a.href = "/home";
+    var evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+                                true, false, false, false, 0, null);
+    a.dispatchEvent(evt);
+}
 </script>
 @stop
--- resources/views/usercontest/show.blade.php
+++ resources/views/usercontest/show.blade.php
@@ -4,23 +4,20 @@
  	<title>
  			
  	</title>
- 	<!-- <meta http-equiv="refresh" content="number;url=http://arena.test/user"> -->
- 	
- </head>
- <body>	
-    <div id="ry">
-    	<?php echo(date("Y-m-d-H-i-s")); ?>
-    	
-    </div>
-	<script type="text/javascript">
-    // init_reload();
-    // function init_reload(){
-    //     setInterval( function() {
-    //                window.location.reload();
+ 	<meta http-equiv="refresh" content="number;url=http://arena.test/user">
+ 	<script type="text/javascript">
+    init_reload();
+    function init_reload(){
+        setInterval( function() {
+                   window.location.reload();
  
-    //       },1000);
-    // }
-    </script>	
+          },1000);
+    }
+    </script>
+ </head>
+ <body>
+			
+	<?php 	var_dump($CQ); ?> 	
  </body>
  </html>
 
