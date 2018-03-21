6c053dbb0ee419dd8e9a119f30b77e133d53ebb4
ScaryLittleOne <ngToan1107@gmail.com>
2018-03-21 10:27:47 +0700

    Fix Querry Bug

--- app/Http/Controllers/user_contest_controller.php
+++ app/Http/Controllers/user_contest_controller.php
@@ -17,25 +17,27 @@ class user_contest_controller extends Controller
     }
     public function index(){
         //Lay ra cau hoi voi cac dieu kien 1. Contest active=1 2. id cua contest = questions.contest_id
-        $CQ = Question::select('questions.*','contests.id')  //Lay Duoc Cau Hoi Hien Tai Dua Tren Bang Contest (Sai )
+        /*$CQ = Question::select('questions.*','contests.id')  //Lay Duoc Cau Hoi Hien Tai Dua Tren Bang Contest (Sai )
             ->join('contests', function ($join) {
                 $join->on('questions.id','=','currentquestion_id')
-                ->where('active','=',1)
+                ->where('active','=',true)
                 ;
             }) 
             ->groupBy('questions.id','contests.id')
             ->having('contests.id','=','questions.contest_id')
-            ->get();
-        var_dump($CQ);
-        echo $CQ;
-
+            ->get();*/
+        $C = DB::table('contests')->where('active','=',true)->first();
+        $question = DB::table('questions')->where('id','=',$C->currentquestion_id)
+            ->where('contest_id','=',$C->id)
+            ->first();
         //lay cau tra loi dua vao cau hoi da lay duoc
-        $answers = DB::table('questions_answers')->where('question_id','=',$CQ->questions.id)->get();//Sai
-        echo $answers;
+        $answers = DB::table('questions_answers')
+            ->where('question_id','=',$question->id)
+            ->get();
 
-    	return view('UserContest');
+        return view('UserContest',compact('question'),compact('answers'));
     }
-    public function transmit_answer(Request $request){
+    public function transmit_answer(Request $request){//Gui Dap An Len Table History
         $history = new History;
         $history->user_id=$request['user_id'];
         $history->contest_id=$request['contest_id'];
