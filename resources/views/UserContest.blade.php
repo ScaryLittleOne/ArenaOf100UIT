430c36c92eb646dd2937ea5edc3aa2e46c683f17
conlocxoaythuytrieu <conlocxoaythuytrieu@gmail.com>
2018-03-21 17:25:17 +0700

    da tu dong thay doi cau hoi

--- resources/views/UserContest.blade.php
+++ resources/views/UserContest.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title','Đấu trường 100')
+@section('title','Đấu trường ánh sáng')
 @section('style')
 .dongho{
 padding: 40px 40px;
@@ -9,8 +9,10 @@ padding: 40px 40px;
 @stop
 @section('content')
 
+<script src="{{ '/jquery.min.js'}}"></script>
 <table>
         {!!$question->content!!}
+         
 </table>
 @foreach($answers as $answer)
     <table>     
@@ -20,45 +22,50 @@ padding: 40px 40px;
     
 @endforeach
 
-
-
-<!-- <script src="{{ '/jquery.min.js'}}"></script>
 <center>
 <button class="dongho" id='demnguoc'>
     <span id='dem'></span> 
     <span id='donvi'></span>
 </button>
-<button class="dongho" id='click'>BẮT ĐẦU</button>
 </center>
-<span id="time" style="display: none;"></span>
+ <?php $x=$question->id; ?>
 <script type="text/javascript">
-   $("#click").click(function(e){
-        time_dest = 0;
-        e.preventDefault(); //time_dest thời gian ban đầu
-        $("#time").load("http://arena.test/user #ry");
-        var ryry = setInterval( function() {
-            $("#time").load("http://arena.test/user #ry");
-            if (time_dest == 0) 
-                time_dest = $("#ry").html();
-            time_curr = $("#ry").html();
-            time_dem = time_count(time_dest, time_curr)
-            $("#dem").html(time_dem);
-            if (time_dem<=0) clearInterval(ryry);
+	
+   $(window).ready( function(){
+        time_dest = Date();
+        var ryry = setInterval( function() 
+        {
+            $.ajax({
+		type: 'GET',
+		url: 'http://arena.test/time',
+		success: function (response) 
+		{
+			 var res=response.split("\n");
+
+			 if (res[1]=={!!$question->id!!})
+			 	console.log(res);
+			 else {
+			 	location.reload();
+
+			 }
+
+		}
+	});
           },1000);
 
       function time_count(time_begin, time_curr) {
-        time_limit = 0;
+        time_limit = 10;
         spl_time_begin = time_begin.split("-");
         spl_time_curr = time_curr.split("-");
         time_begin = new Date(spl_time_begin[0],spl_time_begin[1],spl_time_begin[2],spl_time_begin[3],spl_time_begin[4],spl_time_begin[5]);
         time_curr = new Date(spl_time_curr[0],spl_time_curr[1],spl_time_curr[2],spl_time_curr[3],spl_time_curr[4],spl_time_curr[5]);
-        return time_limit + (time_curr - time_begin)/1000;
+        return time_limit - (time_curr - time_begin)/1000;
 
         //...
       }
 });
 
-</script> -->
+</script>
 
 
 @stop
--- resources/views/usercontest/show.blade.php
+++ resources/views/usercontest/show.blade.php
@@ -15,13 +15,7 @@
         
     </div>
     <script type="text/javascript">
-    // init_reload();
-    // function init_reload(){
-    //     setInterval( function() {
-    //                window.location.reload();
- 
-    //       },1000);
-    // }
+    
     </script>   
  </body>
  </html>
