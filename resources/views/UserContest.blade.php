9d7399bf9d9708b8537dc5dc0a5e17f907571ea7
conlocxoaythuytrieu <conlocxoaythuytrieu@gmail.com>
2018-03-21 13:15:58 +0700

    dong ho giao dien

--- resources/views/UserContest.blade.php
+++ resources/views/UserContest.blade.php
@@ -1,1 +1,64 @@
-asashiash
+@extends('layouts.app')
+@section('title','Đấu trường 100')
+@section('style')
+.dongho{
+padding: 40px 40px;
+	border-radius: 70px;
+	font-size: 30px;
+}
+@stop
+@section('content')
+
+<table>
+        {!!$question->content!!}
+</table>
+@foreach($answers as $answer)
+    <table>     
+        {!!$answer->id!!}
+        {!!$answer->content!!}
+    </table>
+    
+@endforeach
+
+
+
+<!-- <script src="{{ '/jquery.min.js'}}"></script>
+<center>
+<button class="dongho" id='demnguoc'>
+    <span id='dem'></span> 
+    <span id='donvi'></span>
+</button>
+<button class="dongho" id='click'>BẮT ĐẦU</button>
+</center>
+<span id="time" style="display: none;"></span>
+<script type="text/javascript">
+   $("#click").click(function(e){
+        time_dest = 0;
+        e.preventDefault(); //time_dest thời gian ban đầu
+        $("#time").load("http://arena.test/user #ry");
+        var ryry = setInterval( function() {
+            $("#time").load("http://arena.test/user #ry");
+            if (time_dest == 0) 
+                time_dest = $("#ry").html();
+            time_curr = $("#ry").html();
+            time_dem = time_count(time_dest, time_curr)
+            $("#dem").html(time_dem);
+            if (time_dem<=0) clearInterval(ryry);
+          },1000);
+
+      function time_count(time_begin, time_curr) {
+        time_limit = 0;
+        spl_time_begin = time_begin.split("-");
+        spl_time_curr = time_curr.split("-");
+        time_begin = new Date(spl_time_begin[0],spl_time_begin[1],spl_time_begin[2],spl_time_begin[3],spl_time_begin[4],spl_time_begin[5]);
+        time_curr = new Date(spl_time_curr[0],spl_time_curr[1],spl_time_curr[2],spl_time_curr[3],spl_time_curr[4],spl_time_curr[5]);
+        return time_limit + (time_curr - time_begin)/1000;
+
+        //...
+      }
+});
+
+</script> -->
+
+
+@stop
--- resources/views/usercontest/show.blade.php
+++ resources/views/usercontest/show.blade.php
@@ -1,10 +1,27 @@
-<table>
-        {!!$question->content!!}
-</table>
-@foreach($answers as $answer)
-    <table>     
-        {!!$answer->id!!}
-        {!!$answer->content!!}
-    </table>
+
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+    <title>
+            
+    </title>
+    <!-- <meta http-equiv="refresh" content="number;url=http://arena.test/user"> -->
     
-@endforeach
+ </head>
+ <body> 
+    <div id="ry">
+        <?php echo(date("Y-m-d-H-i-s")); ?>
+        
+    </div>
+    <script type="text/javascript">
+    // init_reload();
+    // function init_reload(){
+    //     setInterval( function() {
+    //                window.location.reload();
+ 
+    //       },1000);
+    // }
+    </script>   
+ </body>
+ </html>
