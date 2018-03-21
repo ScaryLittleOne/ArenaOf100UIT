d162be7e1a67b4fd171ad99f9f04903dff53ea5a
tks1998 <trankimsen1681998@gmail.com>
2018-03-22 03:35:38 +0700

    thay xem giup e tg v đc chưa

--- app/Http/Controllers/user_contest_controller.php
+++ app/Http/Controllers/user_contest_controller.php
@@ -27,21 +27,70 @@ class user_contest_controller extends Controller
         return view('UserContest',compact('question'),compact('answers'));
     }               
     public function transmit_answer(Request $request){//Gui Dap An Len Table History
-        $history = new History;
+      //test request
+      $request['user_id']=1;
+      $request['contest_id']=1;
+      $request['question_id']=1;
+      $request['questions_answer_id']='A';
+      
+        /*$history = new histories;
         $history->user_id=$request['user_id'];
         $history->contest_id=$request['contest_id'];
         $history->question_id=$request['question_id'];
         $history->questions_answer_id=$request['questions_answer_id'];
-        $history->save();
-        return view('UserContest'); 
+        $history->save(); 
+        */
+        //$user=users::find($request['user_id']);
+        //env('TIME_LIMIT',31);
+        $time=contest::find($request['question_id']);
+        $time_question="2018-12-12 12:12:12";//test time $time->startcurrentquestion; // tg bd cau hoi 
+        $arr2=preg_split("/\ /",$time_question);
+        $arr_day_question=preg_split("/\-/",$arr2[0]);
+        $arr_time_question=preg_split("/\:/",$arr2[1]);
+        $time_answer=now();//tg tra loi
+        $arr=preg_split("/\ /",$time_answer);
+        $arr_day_answer=preg_split("/\-/",$arr[0]);
+        $arr_time_answer=preg_split("/\:/",$arr[1]);
+        //$time_question=$time->startcurrentquestion;//tg tra loi
+        if ($arr_time_question[2]+31<60) $arr_time_question[2]+=31;
+        else {
+            $arr_time_question[2]=31-(60-$arr_time_question[2]);
+            if ($arr_time_question[1]+1<60) $arr_time_question[1]+=1;
+            else {
+                $arr_time_question[1]=0;
+                $arr_time_question[0]+=1;
+            }
+      }
+      if ($arr_time_answer[0]<$arr_time_question[0] ) $x=true;
+      else {
+         if ($arr_time_answer[0]==$arr_time_question[0] ){  
+              if ($arr_time_answer[1]<$arr_time_question[1]) $x=true;
+              else {
+                  if ($arr_time_answer[1]==$arr_time_question[1] ){
+                        if ($arr_time_answer[2]<=$arr_time_question[2]){
+                          $x=true;
+                        }
+                        else $x=false;
+                  }else $x=false;
+              }
+         }
+         else $x=false; 
+      }
+      $user = DB::table('users')->where('id','=',$request['user_id'])->first(); 
+      $answer=Question::find($request['question_id']); 
+      if ($request['questions_answer_id']==$answer['abcd']  && $x){
+            $user->active=true;
+        }
+        else {
+          $user->active=false;
+        }
     }
 
     public function time() {
-        $user=DB::table('contests')->where('active','=',true)->first();   
+      $user=DB::table('contests')->where('active','=',true)->first();   
       echo($user->id."\n");
       echo($user->currentquestion_id."\n");
       echo($user->startcurrentquestion."\n");   
       echo(now()."\n");    
-    //  return view('usercontest');
     }
 }
