b9703fc0d6e4ccb4535305207d2bdab96c8cc183
tks1998 <trankimsen1681998@gmail.com>
2018-03-21 11:43:32 +0700

    sasass

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,12 @@ Route::get('/home','HomeController@index');
 
 Route::resource('questions', 'question_controller');
 
-route::get('contest','user_contest_controller@index');
-<<<<<<< HEAD
+route::get('usercontest','user_contest_controller@index');
 route::get('histories','user_contest_controller@show_histories');
-route::post('contest2','user_contest_controller@receive_answer');
 
-=======
+route::post('contest2','user_contest_controller@receive_answer');
 route::post('contest','user_contest_controller@transmit_answer');
->>>>>>> 6c053dbb0ee419dd8e9a119f30b77e133d53ebb4
+
 
 Route::get('/admincontest','admin_contest_controller@index')->name('/admincontest');
 Route::post('/admincontest','admin_contest_controller@change')->name('/admincontest');
