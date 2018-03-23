0d782bb75655a370756cacf3280e8b21dda39d54
conlocxoaythuytrieu <conlocxoaythuytrieu@gmail.com>
2018-03-23 08:40:28 +0700

    thay doi giao dien layout, user, admin

--- resources/views/ShowStatistic.blade.php
+++ resources/views/ShowStatistic.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
-@section('title','Thống Kê')
+@section('title','Thống Kê Đáp Án')
 @section('content')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
-</head>
-<body>
-
+<div class="dropdown mb-4">
+  <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Quản Trị Viên
+  </button>
+  <div class="dropdown-menu">
+  <a class="dropdown-item" href="/history" > Lịch Sử Trả Lời Đáp Án </a>
+  <a class="dropdown-item" href="/admincontest" > Vào Đấu Trường </a>
+  <div class="dropdown-divider"></div>
+  <a class="dropdown-item" href="/users">Quản Lí Sinh Viên</a>
+  <a class="dropdown-item" href="/questions" > Quản Lí Đề Thi </a>
+</div>
+</div>
 <div class="container">
   <h2 style="color: white">Thống Kê Kết Quả Câu Hỏi</h2>
   <table class="table table-striped table-bordered" style="margin-top: 15px; background: white;">
@@ -43,9 +43,6 @@
     </tbody>
   </table>
 </div>
-@endsection
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
+<script src="{{ asset('js/app.js') }}">
+    </script>
+@stop
