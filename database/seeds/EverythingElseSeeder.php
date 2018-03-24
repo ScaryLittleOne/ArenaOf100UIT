426b9f330993839bf786fdf9b017e4c2636fe4b2
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2018-03-24 14:31:37 +0700

    prune dummy datadata

--- database/seeds/EverythingElseSeeder.php
+++ database/seeds/EverythingElseSeeder.php
@@ -52,6 +52,22 @@ class EverythingElseSeeder extends Seeder
             'currentquestion_id' => 1,
             'active' => 0
         ]);
+        DB::table('contests')->insert([
+            'currentquestion_id' => 1  ,
+            'active' => 0
+        ]);
+        DB::table('contests')->insert([
+            'currentquestion_id' => 1,
+            'active' => 0
+        ]);
+        DB::table('contests')->insert([
+            'currentquestion_id' => 1  ,
+            'active' => 0
+        ]);
+        DB::table('contests')->insert([
+            'currentquestion_id' => 1,
+            'active' => 0
+        ]);
 
         DB::table('histories')->insert([
             'user_id' => 1,
@@ -61,59 +77,5 @@ class EverythingElseSeeder extends Seeder
 
         ]);
 
-        DB::table('questions')->insert([
-            'content' => 'Ta và Nàng - Đen',
-            'contest_id' => 1
-        ]);
-
-        DB::table('questions_answers')->insert([
-            'abcd' => 'A',
-            'correct' => true,
-            'content' => 'Ɲgàу thì không nghĩ, cứ đêm lại đâm đâm',
-            'question_id' => 2
-        ]);
-
-        DB::table('questions_answers')->insert([
-            'abcd' => 'B',
-            'correct' => false,
-            'content' => 'Đệm êm chăn ấm nó cũng chả hoài năm',
-            'question_id' => 2
-        ]);
-
-        DB::table('questions_answers')->insert([
-            'abcd' => 'C',
-            'correct' => false,
-            'content' => 'Lông bông ngàу tháng đâu chỉ một-hai năm',
-            'question_id' => 2
-        ]);
-
-        DB::table('questions_answers')->insert([
-            'abcd' => 'D',
-            'correct' => false,
-            'content' => 'Ɲhưng đói thì ăn, sợ gì no mà không tiêu',
-            'question_id' => 2
-        ]);
-
-        DB::table('histories')->insert([
-            'user_id' => 2,
-            'contest_id' => 1,
-            'questions_answer_id' => 3,
-            'question_id' => 1,
-
-        ]);
-        DB::table('histories')->insert([
-            'user_id' => 3,
-            'contest_id' => 1,
-            'questions_answer_id' => 1,
-            'question_id' => 1,
-
-        ]);
-        DB::table('histories')->insert([
-            'user_id' => 1,
-            'contest_id' => 2,
-            'questions_answer_id' => 1,
-            'question_id' => 1,
-
-        ]);
     }
 }
